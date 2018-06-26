260ac030b0db73fe757351b2d509b05ecbc4886d
Benjamin Baum <bjb253@cornell.edu>
2018-06-26 01:12:51 -0400

    Add messaging

--- client/index.js
+++ client/index.js
@@ -41,6 +41,12 @@ class App extends React.Component {
         <button className="pull" onClick={this.onPull}>
           Pull Translations
         </button>
+        <div className="messaging">
+          <p>{this.state.translationsStatus === PULL_STATUS.PENDING && 'Pulling...'}</p>
+          <p>{this.state.translationsStatus === PULL_STATUS.SUCCEEDED && 'Translations sent to github!'}</p>
+          <p>{this.state.translationsStatus === PULL_STATUS.FAILED && 'Translations could not be sent. Please try again.'}</p>
+
+        </div>
       </div>
     )
   }
